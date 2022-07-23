795de69c45d1eb718f94ee6b4f15e41d0275cbd3
= <=>
2022-07-23 18:04:11 +0300

    I put a note on you

--- src/components/Dialog/hooks/useAriaHidden.tsx
+++ src/components/Dialog/hooks/useAriaHidden.tsx
@@ -1,11 +1,14 @@
 import { hideOthers, Undo } from 'aria-hidden';
 import { useCallback, useRef } from 'react';
 
+//useCallback with ref and cleanup is still a mystery but I managed to make it work with this article:
+//https://medium.com/welldone-software/usecallback-might-be-what-you-meant-by-useref-useeffect-773bc0278ae
+
 export const useAriaHidden = () => {
   let unHide: Undo;
   const nodeRef = useRef<HTMLElement>(null);
 
-  const setRef = useCallback((newNode: HTMLElement) => {
+  const setOverlayRef = useCallback((newNode: HTMLElement) => {
     if (nodeRef.current) {
       unHide();
     }
@@ -17,5 +20,5 @@ export const useAriaHidden = () => {
     }
   }, []);
 
-  return setRef;
+  return setOverlayRef;
 };
